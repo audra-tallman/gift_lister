f52f31ff1a1d25f1e1b0efc6b29b172d8ee4db9c
Audra <audra.tallman@hotmail.com>
2019-05-11 15:33:38 -0500

    Modify users contoller to inlcude get and post for /login, /logout. Create recipents view.

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,9 +1,12 @@
 class RecipientsController < AppController
 
   get "/recipients" do
-   redirect_if_not_logged_in
-   @recipients = Recipient.all
-   erb :'recipients/index'
+   if logged_in?
+     @recipients = Recipient.all
+     erb :'recipients/index'
+   else
+     redirect to '/login'
+   end
  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,45 @@ class UsersController < AppController
     if !session[:user_id]
       erb :'users/new'
     else
-      redirect to '/that'
+      redirect to '/index'
     end
   end
 
   post '/signup' do
     if params[:username] == "" || params[:password] == ""
-      redirec to '/signup'
+      redirect to '/signup'
     else
       @user = User.create(:username => params[:username], :password =>[:password])
       session[:user_id] = @user.id
-      redirec '/this'
+      redirect to '/recipients'
     end
+  end
+
+  get '/login' do
+    @error_message = params[:error]
+    if !session[:user_id]
+      erb :'users/login'
+    else
+      redirect to '/index'
+  end
+
+  post '/login' do
+   user = User.find_by(:username => params[:username])
+   if user && user.authenticate(params[:password])
+     session[:user_id] = user.id
+     redirect to '/recipients'
+   else
+     redirect to '/signup'
+   end
+  end
+
+  get '/logout' do
+   if session[:user_id] != nil
+     session.destroy
+     redirect to '/login'
+   else
+     redirect to '/'
+   end
   end
 
 end
