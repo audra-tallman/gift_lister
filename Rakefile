f16e306efc8bd8a797b7c469c0e9cb226069d254
Audra <audra.tallman@hotmail.com>
2019-05-11 11:20:06 -0500

    Modify env to establish db connections, create Rakefile, and create app/controllers

---
+++ Rakefile
@@ -0,0 +1,7 @@
+require_relative './config/env'
+
+require 'sinatra/activerecord/rake'
+
+task :console do
+  Pry.start
+end 
---
+++ app/controllers/app_controller.rb
@@ -0,0 +1,6 @@
+class AppController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end 
